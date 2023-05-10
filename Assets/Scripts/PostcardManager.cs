64e93b0b61f1036acc7337b38bbc0e26ed546b34
Lee Jae Hyeong <littleplayer777@gmail.com>
2023-05-10 23:48:59 +0900

    엽서 스크립트 생성, UI 개선
    PostcardManager.cs 생성
    UI 개선, Toggle 과 Dropdown 추가
    (유명세 버프, 길드 등급, 길드 건물, 엽서 구매 단위)

---
+++ Assets/Scripts/PostcardManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostcardManager : MonoBehaviour {
+
+	Sprite Basic;
+	Sprite Luxury;
+	Sprite Dice;
+	Sprite Surprise;
+
+	public string postType;
+	public int honor;
+	public int time;
+	// Use this for initialization
+	void Start () {
+		switch (postType) {
+		case "Basic":
+			honor = 1;
+			time = 30;
+			break;
+		case "Luxury":
+
+			honor = 2;
+			time = 30;
+			break;
+		case "Dice":
+			
+			break;
+		case "Surprise":
+			honor = 42;
+			time = 5;
+			break;
+		}
+	}
+
+	void UIUpdate() {
+		switch (postType) {
+		case "Basic":
+			transform.Find ("Img").GetComponent<Image> ().sprite = Basic;
+			break;
+		case "Luxury":
+			transform.Find ("Img").GetComponent<Image> ().sprite = Luxury;
+			break;
+		case "Dice":
+			transform.Find ("Img").GetComponent<Image> ().sprite = Dice;
+			break;
+		case "Surprise":
+			transform.Find ("Img").GetComponent<Image> ().sprite = Surprise;
+			break;
+		}
+		transform.Find ("Honor").GetComponent<Text> ().text = "+" + honor;
+		transform.Find ("Time").GetComponent<Text> ().text = time + "일";
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
